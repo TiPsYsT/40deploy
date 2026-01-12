fb56eb10629b6a64546007161df8c84b0265f376
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-12 14:09:54 +0100

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,20 +1,21 @@
 import { state } from "./state.js";
 import { spawnUnit } from "./board.js";
 
+// 4.1 init
 export function setupSidebar() {
   renderSidebar();
 }
 
+// 4.2 render
 export function renderSidebar() {
   const list = document.getElementById("unit-list");
   list.innerHTML = "";
 
-  state.armyUnits.forEach((u, i) => {
+  state.armyUnits.forEach(unit => {
     const div = document.createElement("div");
     div.className = "unit";
-    div.innerText = `${u.name} (${u.count})`;
-    div.onclick = () => spawnUnit(u);
+    div.textContent = `${unit.name} (${unit.count})`;
+    div.onclick = () => spawnUnit(unit);
     list.appendChild(div);
   });
 }
-
