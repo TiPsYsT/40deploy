d3881ea1377ad7c9a08f4de0b17f6b114d88865b
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-16 09:20:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,18 @@ import { importNewRecruit } from "./js/importer.js";
 import { renderSidebar } from "./js/sidebar.js";
 import { spawnModel, draw } from "./js/board.js";
 
-document.getElementById("fileInput").onchange = e => {
-  const file = e.target.files[0];
-  const reader = new FileReader();
+reader.onload = () => {
+  const json = JSON.parse(reader.result);
+  console.log("IMPORTERAD JSON:", json);
+
+  const models = importNewRecruit(json);
+  console.log("MODELS:", models);
+
+  setModels(models);
+  renderSidebar(spawnModel);
+  draw();
+};
+
 
   reader.onload = () => {
     const json = JSON.parse(reader.result);
