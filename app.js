88921da577c46c97517eadf822f16749fe014376
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-16 08:59:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-// 1.1 imports
-import { setupBoard } from "./js/board.js";
-import { setupSidebar } from "./js/sidebar.js";
-import { setupImporter } from "./js/importer.js";
+import { setModels } from "./js/state.js";
+import { importNewRecruit } from "./js/importer.js";
+import { renderSidebar } from "./js/sidebar.js";
+import { spawnModel, draw } from "./js/board.js";
 
-// 1.2 init
-setupBoard();
-setupSidebar();
-setupImporter();
+document.getElementById("fileInput").onchange = e => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = () => {
+    const json = JSON.parse(reader.result);
+    const models = importNewRecruit(json);
+    setModels(models);
+    renderSidebar(spawnModel);
+    draw();
+  };
+
+  reader.readAsText(file);
+};
