58b82d8438d1e0a98bdf75ac5bb7b7e2499f39bb
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-18 20:14:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
 import { setModels } from "./js/state.js";
 import { importNewRecruit } from "./js/importer.js";
 import { renderSidebar } from "./js/sidebar.js";
-import { draw, spawnModel } from "./js/board.js";
+import { redrawBoard, spawnModel } from "./js/board.js";
 import { loadBases } from "./js/baseResolver.js";
-
-console.log("APP STARTAR");
+import { loadMission } from "./js/missionLoader.js";
+import { loadTerrain } from "./js/terrainLoader.js";
 
 const fileInput = document.getElementById("fileInput");
+const missionSelect = document.getElementById("missionSelect");
+const terrainSelect = document.getElementById("terrainSelect");
+
+let currentMission = null;
+let currentTerrain = null;
 
 (async function init() {
   await loadBases();
-  console.log("BASES LADDADE");
 })();
 
 fileInput.addEventListener("change", e => {
-  console.log("FIL VALD");
-
   const file = e.target.files[0];
   const reader = new FileReader();
 
   reader.onload = e => {
-    console.log("FILE READ OK");
-
     const json = JSON.parse(e.target.result);
     const models = importNewRecruit(json);
 
     setModels(models);
     renderSidebar(spawnModel);
-    draw();
+    redrawBoard(currentMission, currentTerrain);
   };
 
   reader.readAsText(file);
+});
+
+missionSelect.addEventListener("change", async e => {
+  currentMission = e.target.value
+    ? await loadMission(e.target.value)
+    : null;
+
+  redrawBoard(currentMission, currentTerrain);
+});
+
+terrainSelect.addEventListener("change", async e => {
+  currentTerrain = e.target.value
+    ? await loadTerrain(e.target.value)
+    : null;
+
+  redrawBoard(currentMission, currentTerrain);
 });
