72f6bb8fdf5b48389998ba9b729c1ce2d4809e36
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-18 11:24:58 +0100

    Create deployments.js

---
+++ overlays/deployments.js
@@ -0,0 +1,21 @@
+export const deployments = {
+  dawnOfWar: {
+    name: "Dawn of War",
+    zones: [
+      {
+        side: "blue",
+        x: 0,
+        y: 0,
+        w: 1524,
+        h: 305 // 12"
+      },
+      {
+        side: "red",
+        x: 0,
+        y: 1118 - 305,
+        w: 1524,
+        h: 305
+      }
+    ]
+  }
+};
