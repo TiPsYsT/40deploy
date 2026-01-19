46224059d57c5f5108251ddb0a8c309f02964c01
TiPsYsT <alexanderlundgren@hotmail.com>
2026-01-19 11:13:45 +0100

    Update terrainLoader.js

--- js/terrainLoader.js
+++ js/terrainLoader.js
@@ -1,1 +1,9 @@
-return fetch(`terrain/${id}.json`).then(r => r.json());
+export async function loadTerrain(id) {
+  const res = await fetch(`terrain/${id}.json`);
+
+  if (!res.ok) {
+    throw new Error(`Failed to load terrain: ${id}`);
+  }
+
+  return await res.json();
+}
